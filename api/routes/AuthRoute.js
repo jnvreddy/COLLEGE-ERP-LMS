4b9eb0c0825d5bb5ee7464fc9fb50660b2a1a0ef
jnvreddy <jnvreddyofficial@gmail.com>
2024-06-05 06:54:36 +0530

    Fully functionalLogin page with navbar and profile page with additional features

--- api/routes/AuthRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login } from "../controllers/AuthController.js";
-
-const router = express.Router();
-
-router.post("/Login", login)
-
-export default router;
--- api/routes/UserRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { test } from '../controllers/UserController.js';
-
-const router = express.Router();
-
-router.get('/', test);
-
-export default router;
